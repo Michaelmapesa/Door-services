e2778528dbc22ef55f9cf653e855719d9ef7c4d7
michael mapesa <michaelmapesa93@gmail.com>
2021-11-18 14:53:26 +0300

    add modification

--- validatesignup.js
+++ validatesignup.js
@@ -28,47 +28,7 @@ lastname.addEventListener('keyup',function(){
     }
 })
 
-let location = document.querySelector('#location');
-location.addEventListener('keyup',function(){
-    var u_times = document.querySelector('.u_times');
-    var u_check = document.querySelector('.u_check');
-    if (location.value.length== 0 || location.value.length < 8){
-        location.style.border='3px solid red';
-        u_times.style.display = 'block';
-        u_check.style.display = 'none';
-    } else{
-        location.style.border = "3px solid green";
-        u_times.style.display = 'none';
-        u_check.style.display = 'block';
-    }
-})
 
-let email = document.querySelector('#email');
-email.addEventListener('keyup',function(){
-    var u_times = document.querySelector('.u_times');
-    var u_check = document.querySelector('.u_check');
-    if (email.value.length== 0 || email.value.length < 8){
-        email.style.border='3px solid red';
-        u_times.style.display = 'block';
-        u_check.style.display = 'none';
-    } else{
-        email.style.border = "3px solid green";
-        u_times.style.display = 'none';
-        u_check.style.display = 'block';
-    }
-})
 
-let areacode = document.querySelector('#areacode');
-areacode.addEventListener('keyup',function(){
-    var u_times = document.querySelector('.u_times');
-    var u_check = document.querySelector('.u_check');
-    if (areacode.value.length== 0 || areacode.value.length < 8){
-        areacode.style.border='3px solid red';
-        u_times.style.display = 'block';
-        u_check.style.display = 'none';
-    } else{
-        areacode.style.border = "3px solid green";
-        u_times.style.display = 'none';
-        u_check.style.display = 'block';
-    }
-})
+
+
